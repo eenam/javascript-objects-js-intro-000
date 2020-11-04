40769770299a974db7ee297ecc316241e6c5b552
Emma Nam <en2458@gmail.com>
2020-11-04 02:42:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,8 +4,7 @@ var playlist = {artistName: 'songTitle'};
 // The body of the function should add the song and artist as a key-value pair to the playlist object.
 // The function should return the whole playlist.
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, artistName, songTitle)
-
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
 
